ac5e6fa27ace5d4bb8a2794d00437110d4566bdc
Davcid1572 <ifejiofordavid1572@gmail.com>
2025-08-20 12:00:38 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Signin from "./Pages/Signin";
+import Dashboard from "./Pages/Dashboard";
 
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
