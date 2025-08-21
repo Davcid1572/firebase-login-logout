dbe701751ff71249807add11244b30c3ebe75855
Davcid1572 <ifejiofordavid1572@gmail.com>
2025-08-21 08:55:50 +0100

    .

--- src/Component/SigninSignout/index.jsx
+++ src/Component/SigninSignout/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./styles.css";
+import Input from "../Input";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <div className="Signup-wrapper">
+      <h2>
+        Sign Up <span style={{ color: "var(--theme)" }}>ChiFinance</span> Using
+        Email
+      </h2>
+      <form action="">
+        <Input />
+      </form>
+    </div>
+  );
 };
 
 export default index;
--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Headers from "../Component/Header/index.jsx";
+import SignSignout from "../Component/SigninSignout";
 
 const Signin = () => {
   return (
     <div>
       <Headers />
+      <div className="wrapper">
+        <SignSignout />
+      </div>
     </div>
   );
 };
